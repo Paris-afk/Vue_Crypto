70e500944e4e9adbf5a5526347465072b5eeed54
Paris López <paris.obed@hotmail.com>
2025-06-05 19:55:10 +0200

    fix(master): change route

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 // Configuración para GitHub Pages
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Vue_Crypto/' // Nombre del repositorio
-    : '/'
+    : '/',
+  
+  configureWebpack: {
+    plugins: [
+      // Asegura que los archivos .nojekyll y 404.html se copien al directorio dist
+      new CopyPlugin([
+        { from: path.resolve(__dirname, 'public/.nojekyll'), to: '' },
+        { from: path.resolve(__dirname, 'public/404.html'), to: '' }
+      ])
+    ]
+  }
 }
